dfd4ff5b631f9d7a600eabb1bc21cb2034c2977b
kuzmychAndrew <89782153+kuzmychAndrew@users.noreply.github.com>
2022-10-26 14:58:32 +0300

    Initial Commit

---
+++ TextRecognition/TextRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognitionApp.swift
+//  TextRecognition
+//
+//  Created by Андрій Кузьмич on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
